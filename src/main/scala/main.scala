867086a8f271c0e5ca2ddbda055ac10aaad3c28e
Mikolaj Kapica <mikolajkapica@mail.com>
2023-08-28 21:40:00 +0200

    add simple file input handling to main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,12 @@
+import scala.annotation.tailrec
+
 @main
 def main(): Unit = {
+  val source = scala.io.Source.fromFile("src/program.txt")
+  val code = try source.mkString finally source.close()
+
+  val l = Lexer(code)
+  val tokens = l.getTokens(l)
 }
+
+
