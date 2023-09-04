2fc27e99e1d9c86d87f7966aa158e425e20602ce
Mikolaj Kapica <mikolajkapica@mail.com>
2023-09-04 17:26:31 +0200

    update main and program

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,11 +2,27 @@ import scala.annotation.tailrec
 
 @main
 def main(): Unit = {
-  val source = scala.io.Source.fromFile("src/program.txt")
-  val code = try source.mkString finally source.close()
-
-  val l = Lexer(code)
-  val tokens = l.getTokens(l)
+  def repl: Unit = {
+    val source = scala.io.Source.fromFile("src/program.txt")
+    val code = try source.mkString finally source.close()
+    code.split("\n") foreach { line =>
+      if (line != "") {
+        print(line + " = ")
+        val tokens = Lexer(line).getTokens()
+        val ast = Parser().getAst(tokens)
+        Evaluator().eval(ast)
+      }
+    }
+  }
+  repl
 }
 
 
+def prettyPrintAst(ast: List[Node]) = {
+  def prettyPrintAstNode(node: Node, indent: Int): Unit = {
+    println("  " * indent + node.nodeType + " " + node.literal)
+    node.children.foreach(prettyPrintAstNode(_, indent + 1))
+  }
+  ast.foreach(prettyPrintAstNode(_, 0))
+
+}
